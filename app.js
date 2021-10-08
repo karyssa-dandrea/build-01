40ae1358a6d80398d103158068b03b09a83c100e
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-08 13:57:13 -0700

    created html and javascript for a generator button

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
-
+const generateButton = document.getElementById('generate');
+const answerDisplay = document.getElementById('answer-display');
 // initialize global state
-
 // set event listeners 
+const randomNum = ()=>{
+    return Math.ceil(Math.random() * 100);
+};
+generateButton.addEventListener('click', ()=>{
+    answerDisplay.textContent = answerDisplay[randomNum];
+});
+
+console.log('click the button');
+
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
